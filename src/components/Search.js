91e868b8c7653af4ef242f843f5875f3d2c23632
Vlad <vlad@appliedlabs.io>
2019-11-14 16:32:54 +0500

    Complete Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,27 +2,55 @@ import React from 'react';
 import intrinio from '../apis/intrinio';
 
 class Search extends React.Component {
-	state = { term: '', companies: []	};
+	state = { term: '', companies: []	}
 
-	componentDidMount() {
-		this.SearchLoading();
+	debounce = (fn, delay) => {
+      let timer = null;
+      return function (...args) {
+          const context = this;
+          timer && clearTimeout(timer);
+          timer = setTimeout(() => {
+              fn.apply(context, args);
+          }, delay);
+      };
 	}
 
 	SearchLoading = async () => {
 		const response = await intrinio.get('/companies/search', {
 			params: {
-				query: 'APSOQ'
+				query: this.state.term
 			}
 		});
 
-		this.setState({ 
-			companies: response
-		});
-	};
+		if (response.data.companies[0] !== undefined) {
+			this.setState({ 
+				companies: response.data.companies[0]
+			});
+		}
+	}
+	constructor(props) {
+      super(props);
+      this.SearchLoading = this.debounce(this.SearchLoading, 200);
+  }
+
+  handleTermChange = (e) => {
+		this.setState({companies: []});
+    this.setState({term: e.target.value});
+    this.SearchLoading();
+  }
+  
 
 	render() {
-		console.log(this.state.companies);
-		return <div className="ui container">Search</div>;
+		return (
+			<div className="ui container">
+				<div className="ui icon input">
+				  <input type="text" placeholder="Search..." value={this.state.term} onChange={this.handleTermChange} />
+				  <div>
+				  	Result: {this.state.companies.name}
+				  </div>
+				</div>
+			</div>
+		);
 	}
 }
 
